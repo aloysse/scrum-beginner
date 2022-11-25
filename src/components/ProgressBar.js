82629f2a0b457168781c2bcdecf536e903830ec3
aloysse <shiou.ho@gmail.com>
2022-11-25 22:30:36 +0800

    [add] progress bar component

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <div className="w-[480px] bg-barContainer mx-3">
+      <div className=" h-[8px] w-[50%] bg-P1"></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
