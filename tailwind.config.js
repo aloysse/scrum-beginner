21a28ba95cc048b7dc6e6f7101b2cc07bb704a41
aloysse <shiou.ho@gmail.com>
2022-11-25 22:29:29 +0800

    customize style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    letterSpacing: {
+      tightest: "-.075em",
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".25em",
+    },
+    extend: {
+      backgroundImage: {
+        main: "url('assets/img/bg.svg')",
+        secondary: "url('assets/img/bg2.svg')",
+        btn: "url('assets/img/btn-bg.svg')",
+        cardBack: "url('assets/img/card-back.svg')",
+        cardContent: "url('assets/img/card-content.svg')",
+        placeBg: "url('assets/img/place-bg.svg')",
+        alertBg: "url('assets/img/alert-bg.svg')",
+        cardStar: "url('assets/img/card-star.svg')",
+        flow: "url('assets/img/flow.svg')",
+      },
+      colors: {
+        P1: "#D7B676",
+        P2: "#DBC087",
+        P3: "#00022B",
+        barContainer: "rgba(215, 182, 118, 0.3);",
+        blackTrans: "rgba(1,1,1,0.5)",
+      },
+    },
   },
   plugins: [],
 };
